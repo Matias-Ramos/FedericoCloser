0f41df684b846b360e54d8ee016e97dcf65bdaf8
Matias <matiasramos>
2024-01-22 13:33:28 -0300

    cleaner code

--- src/pages/header/NavHeader.tsx
+++ src/pages/header/NavHeader.tsx
@@ -1,21 +1,30 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+// BTS
+import { Container, Row, Col } from 'react-bootstrap';
+// Components
 import Logo from '../../components/Logo';
 import BtnGroup from './BtnGroup';
+// Style
+const rowStyle='d-flex justify-content-center justify-content-lg-between'
+const btnGroupColStyle = 'd-flex justify-content-center flex-column '
+const btnGroupSpanStyle = 'd-block text-center text-lg-start d-md-inline '
 
 const NavHeader = () => {
   return (
     <header>
         <Container fluid>
-            <Row className='d-flex justify-content-center justify-content-lg-between'>
+            <Row className={rowStyle}>
+
                 <Col xs={5} md={3}>
                     <Logo />
                 </Col>
-                <Col xs={12} md={8} lg={9} className='d-flex justify-content-center flex-column '>
-                    <span className='d-block text-center text-lg-start d-md-inline '>Acerca de:</span>
+
+                <Col className={btnGroupColStyle} xs={12} md={8} lg={9}>
+                    <span className={btnGroupSpanStyle}>
+                        Acerca de:
+                    </span>
                     <BtnGroup />
                 </Col>
+
             </Row>
         </Container>
     </header>

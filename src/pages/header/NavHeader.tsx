1016dd809f9d280a3e035c79826a5b4d0777c644
Matias <matiasramos>
2024-01-23 13:47:19 -0300

    Products.tsx implemented and responsive. NavHeader.tsx font-style and flexbox corrected

--- src/pages/header/NavHeader.tsx
+++ src/pages/header/NavHeader.tsx
@@ -5,22 +5,22 @@ import Logo from '../../components/Logo';
 import BtnGroup from './BtnGroup';
 // Style
 const rowStyle='d-flex justify-content-center justify-content-lg-between'
-const btnGroupColStyle = 'd-flex justify-content-center flex-column '
+const btnGroupColStyle = 'd-flex justify-content-center flex-column ps-lg-5'
 const btnGroupSpanStyle = 'd-block text-center text-lg-start d-md-inline '
 
 const NavHeader = () => {
   return (
     <header>
-        <Container fluid>
+        <Container>
             <Row className={rowStyle}>
 
                 <Col xs={5} md={2}>
                     <Logo />
                 </Col>
 
-                <Col className={btnGroupColStyle} xs={12} md={8} lg={9}>
+                <Col className={btnGroupColStyle} xs={12} md={7} lg={6} xxl={5}>
                     <span className={btnGroupSpanStyle}>
-                        Acerca de:
+                        Estas viendo:
                     </span>
                     <BtnGroup />
                 </Col>

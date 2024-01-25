fab17120c675d4a73e1f3f4a92d66c4b01f3a775
Matias <matiasramos>
2024-01-25 15:00:09 -0300

    Testimonials copied and paste to reach deadline. Testimonials will be moved to the components dir in the future as it is a reusable component

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import NavHeader from './pages/cdv/header/NavHeader'
 import Leaders from './pages/cdv/leaders/Leaders'
 import Products from './pages/cdv/products/Products'
 import Purpose from './pages/cdv/purpose/Purpose'
-import Testimonials from './pages/cdv/testimonials/Testimonials'
+import TestimonialsCdv from './pages/cdv/testimonials/Testimonials'
 import About from './pages/fede/aboutFede/About'
 import Intro from './pages/fede/introduction/Intro'
 import Pitch from './pages/fede/pitch/Pitch'
+import Testimonials from './pages/fede/testimonials/Testimonials'
 
 const App = () => {
   return (
@@ -17,10 +18,11 @@ const App = () => {
     <Detail />
     <Leaders />
     <Products />
-    <Testimonials />
+    <TestimonialsCdv />
     <Footer />
     <Intro />
     <About />
+    <Testimonials />
     <Pitch />
     </>
   )

dabd1020893229fd16fc0ae00feb4d8a3f287914
Matias <matiasramos>
2024-01-29 07:03:40 -0300

    Testimonials.tsx moved to components directory

--- src/pages/cdv/testimonials/testimonialList.ts
+++ src/pages/cdv/testimonials/testimonialList.ts
@@ -1,19 +1,4 @@
-export class Testimonial {
-  private static _id: number = 0;
-
-  id: number;
-  entity: string;
-  data: string[];
-
-  incrementId() { Testimonial._id++; }
-
-  constructor(entity: string, data: string[]) {
-    this.id = Testimonial._id;
-    this.entity = entity;
-    this.data = data;
-    this.incrementId();
-  }
-}
+import { Testimonial } from "../../../classes/testimonialType"
 
 const testimonialOne = new Testimonial(
   'Bienes raices',

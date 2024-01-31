54eafd3597c6cb78d45475732d4285da2f4adc6a
Matias <matiasramos>
2024-01-31 18:38:47 -0300

    Desktop view styled. Clean code.

--- src/pages/cdv/detail/Detail.tsx
+++ src/pages/cdv/detail/Detail.tsx
@@ -1,40 +1,39 @@
 // BTS
 import { Container, Row, Col } from 'react-bootstrap';
 // Components
-// import Text from './Text';
 import CallToAction from './CallToAction';
 import CardMapper from './CardMapper';
+import Text from './Text';
 // Styling
-const txtStyle = 'd-flex justify-content-start';
+const rowStyle = 'd-flex justify-content-start';
 const glassmorphism = {
   background: 'rgba(74, 144, 226, 0)',
   boxShadow: '0 3px 5px 0 rgba(31, 38, 135, 0.37)',
-  backdropFilter: 'blur(1px)',
+  backdropFilter: 'blur(3px)',
   border: '1px solid rgba(255, 255, 255, 0.18)',
 };
-const firstColStyle = "d-flex flex-column justify-content-center"
-
+const txtStyle = "d-flex flex-column justify-content-center"
+const ctaStyle = 'd-flex align-items-center justify-content-center mt-4'
 
 const Detail = () => {
   return (
-    <section id="detail">
-        <Container>
-            <Row className={txtStyle}>
+    <section id="detail" style={glassmorphism}>
+        <Container >
+            <Row className={rowStyle}>
 
-              <Col xs={12} lg={7} className={firstColStyle}>
-                  <h2>[Detalle]</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem non magnam, voluptatibus cum quaerat adipisci ratione ab impedit facere tempore deserunt ducimus pariatur, provident.</p>
+              <Col xs={12} lg={7} className={txtStyle}>
+                <Text />
               </Col>
 
               <Col xs={12}>
                 <CardMapper />
               </Col>
 
-              <Col xs={12} style={glassmorphism} className='d-flex align-items-center justify-content-center mt-4'>
+              <Col xs={12} className={ctaStyle}>
                 <CallToAction />
               </Col>
-            </Row>
 
+            </Row>
         </Container>
     </section>
   )

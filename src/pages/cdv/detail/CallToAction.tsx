b95ce87d9f2a18c068bea187c3501ce9c98efcd0
Matias <matiasramos>
2024-02-09 20:02:06 -0300

    2nd checkpoint CtaBtn new model

--- src/pages/cdv/detail/CallToAction.tsx
+++ src/pages/cdv/detail/CallToAction.tsx
@@ -1,34 +1,32 @@
 // BTS Grid
 import { Col, Row } from "react-bootstrap"
 // Components
-import CtaBtn from "../../../components/CtaBtn";
 import Chevron from "../../../components/chevron/Chevron";
+import { Button } from "@mui/material";
 // Styles
-const rowStyle= "align-items-center py-4 py-lg-0 bg-light w-100 blueBorder"
-const spanStyle= "d-block text-center"
+const rowStyle = "align-items-center py-4 py-lg-0 bg-light w-100 blueBorder"
+const spanStyle = "d-block text-center"
 const ctaStyle = "d-flex justify-content-center"
 
 const CallToAction = () => (
-        <Row className={rowStyle}>
+    <Row className={rowStyle}>
 
-            <Col xs={12} lg={5} >
-                <span className={spanStyle}>
-                    ¿ Querés saber más ?
-                </span>
-            </Col>
+        <Col xs={12} lg={5} >
+            <span className={spanStyle}>
+                ¿ Querés saber más ?
+            </span>
+        </Col>
 
-            <Col xs={12} lg={2} >
-                <Chevron />
-            </Col>
+        <Col xs={12} lg={2} >
+            <Chevron />
+        </Col>
 
-            <Col xs={12} lg={5} className={ctaStyle}>
-                <CtaBtn
-                    text="Descargar PDF"
-                    variant="outlined"
-                    handleClick={() => { }}
-                />
-            </Col>
-        </Row>
+        <Col xs={12} lg={5} className={ctaStyle}>
+            <Button variant="outlined" onClick={() => { }}>
+                Descargar PDF
+            </Button>
+        </Col>
+    </Row>
 
 )
 export default CallToAction

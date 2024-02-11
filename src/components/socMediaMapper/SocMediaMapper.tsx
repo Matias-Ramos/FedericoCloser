7edfa6639f51f7938aae0c629273cd26b84f037a
Matias <matiasramos>
2024-02-11 20:32:30 -0300

    checkpoint for staggerFadeIn modification so it receives params

--- src/components/socMediaMapper/SocMediaMapper.tsx
+++ src/components/socMediaMapper/SocMediaMapper.tsx
@@ -4,9 +4,19 @@ import { FaXTwitter, FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa6";
 const containerStyle = 'd-flex flex-row justify-content-evenly my-3 socMediaCtr';
 // Types
 import { Member_T } from "./MembersType";
+// Animation
+import { motion } from 'framer-motion'
+import { staggerFadeIn } from "../../pages/variants";
 
 const SocMediaMapper = ({ member }: { member: Member_T }) => (
-  <div className={containerStyle}>
+  <motion.div
+    className={containerStyle}
+    variants={staggerFadeIn.container}
+    initial="hidden"
+    animate="show"
+    viewport={{ once: true }}
+
+  >
     {Object.entries(member.socialMedia).map(([social, link]) => (
       <a
         key={social}
@@ -14,12 +24,40 @@ const SocMediaMapper = ({ member }: { member: Member_T }) => (
         target="_blank"
         rel="noopener noreferrer"
       >
-        {social === 'fb' && <FaFacebook />}
-        {social === 'tiktok' && <FaTiktok />}
-        {social === 'twitter' && <FaXTwitter />}
-        {social === 'instagram' && <FaInstagram />}
+        {social === 'fb' &&
+          <motion.div
+            variants={staggerFadeIn.item}
+            viewport={{ once: true }}
+          >
+            <FaFacebook />
+          </motion.div>
+        }
+        {social === 'tiktok' &&
+          <motion.div
+            variants={staggerFadeIn.item}
+            viewport={{ once: true }}
+          >
+            <FaTiktok />
+          </motion.div>
+        }
+        {social === 'twitter' &&
+          <motion.div
+            variants={staggerFadeIn.item}
+            viewport={{ once: true }}
+          >
+            <FaXTwitter />
+          </motion.div>
+        }
+        {social === 'instagram' &&
+          <motion.div
+            variants={staggerFadeIn.item}
+            viewport={{ once: true }}
+          >
+            <FaInstagram />
+          </motion.div>
+        }
       </a>
     ))}
-  </div>
+  </motion.div>
 )
 export default SocMediaMapper

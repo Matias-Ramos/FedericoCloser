b36ac1d2cf94eaea24576b93d28140721d0a96d3
Matias <matiasramos>
2024-02-12 15:02:59 -0300

    footer fixed

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,29 +4,36 @@ import { Container, Row, Col } from "react-bootstrap"
 import { FaFacebook, FaInstagram, FaTiktok, FaTwitter } from "react-icons/fa"
 // Components
 import Links from "./Links"
+import { useLocation } from "react-router-dom"
 // Style
 const footerStyle = "bg-dark text-light p-4"
-const rowStyle = "mt-5 pt-5 mt-lg-0 "
-const colStyle = "d-flex flex-column"
-const iconsCtStyle = "d-flex justify-content-center justify-content-lg-end align-items-lg-center gap-3"
+const colStyle = "d-flex flex-column text-lg-start mb-3 mb-lg-0"
+const iconsCtStyle = "d-flex justify-content-start justify-content-lg-end align-items-lg-center gap-3"
 
-const Footer = () => (
-  <footer className={footerStyle}>
-    <Container>
-      <Row className={rowStyle}>
+const Footer = () => {
+  
+  const location = useLocation();
+  const rowStyle = `${location.pathname==="/cdv" ? "mt-5 pt-5" : "pt-4"} mt-lg-0`
 
-        <Col className={colStyle}>
-          <Links />
-        </Col>
+  return(
+    <footer className={footerStyle}>
+      <Container>
+        <Row className={rowStyle}>
 
-        <Col className={iconsCtStyle}>
-          <FaFacebook />
-          <FaTwitter />
-          <FaTiktok />
-          <FaInstagram />
-        </Col>
-      </Row>
-    </Container>
-  </footer>
-)
+          <Col xs={12} lg={6} className={colStyle}>
+            <Links />
+          </Col>
+
+          <Col xs={12} lg={6} className={iconsCtStyle}>
+            <FaFacebook />
+            <FaTwitter />
+            <FaTiktok />
+            <FaInstagram />
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
 export default Footer
